b88be12dd65b5ed883369e55331dd0fda7ed1c92
arifhossain_cse22 <arifaka555@gmail.com>
2024-09-03 23:35:41 +0600

    Update python code

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,109 +1,110 @@
-"""An AWS Python Pulumi program"""
+""" AWS Python Pulumi Program """
 
 import pulumi
-import pulumi_aws as aws 
+import pulumi_aws as aws
 import pulumi_aws.ec2 as ec2
-from pulumi_aws.ec2 import SecurityGroupRuleArgs
-
+import os
 
 # Configuration
-config = pulumi.Config() 
+config = pulumi.Config()
 instance_type = 't2.micro' 
 ami = "ami-01811d4912b4ccb26"
+vpc_cidr_block = "10.0.0.0/16"
+public_subnet_cidr_block = "10.0.1.0/24"
+private_subnet_cidr_block = "10.0.2.0/24"
+availability_zone = "ap-southeast-1a"
 
-
-# Add VPC
-vpc = aws.ec2.Vpc("my-vpc",
-    cidr_block="10.10.0.0/16",
-    enable_dns_hostnames=True,
+# Create a VPC
+vpc = ec2.Vpc("my-vpc",
+    cidr_block=vpc_cidr_block,
     enable_dns_support=True,
+    enable_dns_hostnames=True,
     tags={
-        "Name": "my-vpc-for-k3s"
+        "Name": "my-vpc"
     }
 )
 
-
-# Add public subnet
+# Create a Public Subnet
 public_subnet = ec2.Subnet("public-subnet",
     vpc_id=vpc.id,
-    cidr_block="10.10.1.0/24",
-    map_public_ip_on_launch=True,  # Typically True for public subnets
-    availability_zone="ap-southeast-1a",
+    cidr_block=public_subnet_cidr_block,
+    map_public_ip_on_launch=True,
+    availability_zone=availability_zone,
     tags={
         "Name": "public-subnet",
     }
 )
 
-# Add private subnet
-private_subnet = ec2.Subnet('private-subnet',
+# Create a Private Subnet
+private_subnet = ec2.Subnet("private-subnet",
     vpc_id=vpc.id,
-    cidr_block='10.10.2.0/24',
-    map_public_ip_on_launch=False, 
-    availability_zone='ap-southeast-1a',
+    cidr_block=private_subnet_cidr_block,
+    map_public_ip_on_launch=False,
+    availability_zone=availability_zone,
     tags={
         "Name": "private-subnet",
     }
 )
 
-
-# Adding the Internet Gateway with a Name tag
-igw = ec2.InternetGateway("Internet-Gateway",
+# Create an Internet Gateway
+igw = ec2.InternetGateway("internet-gateway",
     vpc_id=vpc.id,
     tags={
         "Name": "my-igw"
     }
 )
 
-# Create Route Table for the public subnet
+# Create a Route Table for the Public Subnet
 public_route_table = ec2.RouteTable("public-route-table",
     vpc_id=vpc.id,
     routes=[{
         "cidr_block": "0.0.0.0/0", 
         "gateway_id": igw.id,
     }],
+    tags={
+        "Name": "public-route-table"
+    }
 )
 
-# Associate Route Table with the Public Subnet
-public_route_table_association = ec2.RouteTableAssociation('public-route-table-association',
+# Associate the Route Table with the Public Subnet
+public_route_table_association = ec2.RouteTableAssociation("public-route-table-association",
     subnet_id=public_subnet.id,
     route_table_id=public_route_table.id
 )
 
+# Create an Elastic IP for the NAT Gateway
+eip = ec2.Eip("nat-eip", vpc=True)
 
-# Elastic IP for NAT Gateway 
-eip = ec2.Eip('nat-eip', 
-    vpc=True,
-    domain="vpc",  # Explicitly setting the domain attribute to "vpc"
+# Create a NAT Gateway in the Public Subnet
+nat_gateway = ec2.NatGateway("nat-gateway",
+    subnet_id=public_subnet.id,
+    allocation_id=eip.id,
+    tags={
+        "Name": "my-nat-gateway"
+    }
 )
 
-# NAT Gateway
-nat_gateway = ec2.NatGateway(
-    'nat-gateway',
-    subnet_id=public_subnet.id, 
-    allocation_id=eip.id
-) 
-
-
-# Route Table for the Private Subnet 
-private_route_table = ec2.RouteTable(
-    'private-route-table', 
-    vpc_id=vpc.id, 
+# Create a Route Table for the Private Subnet
+private_route_table = ec2.RouteTable("private-route-table",
+    vpc_id=vpc.id,
     routes=[{
-        'cidr_block': '0.0.0.0/0', 
-        'nat_gateway_id': nat_gateway.id,
-    }]
+        "cidr_block": "0.0.0.0/0", 
+        "nat_gateway_id": nat_gateway.id,
+    }],
+    tags={
+        "Name": "private-route-table"
+    }
 )
 
-# Associate the private route table with the private subnet 
-private_route_table_association = ec2.RouteTableAssociation(
-    'private-route-table-association', 
-    subnet_id=private_subnet.id, 
+# Associate the Route Table with the Private Subnet
+private_route_table_association = ec2.RouteTableAssociation("private-route-table-association",
+    subnet_id=private_subnet.id,
     route_table_id=private_route_table.id
 )
 
-# (Optional) Output the IDs of created resources
+# Output the IDs of the created resources
 pulumi.export('vpc_id', vpc.id)
 pulumi.export('public_subnet_id', public_subnet.id)
 pulumi.export('private_subnet_id', private_subnet.id)
-pulumi.export('igw_id', igw.id)
+pulumi.export('internet_gateway_id', igw.id)
 pulumi.export('nat_gateway_id', nat_gateway.id)
